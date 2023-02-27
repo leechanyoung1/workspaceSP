b04908f4677f4fcd1500e11445c0aa31aecb1c18
itwill <itwill@BOOK-UI59MD6R98>
2023-02-27 11:15:49 +0900

    ,

---
+++ spring_application_mybatis/src/main/java/com/mybatis3/dao/mapper/StudentMapper.java
@@ -0,0 +1,11 @@
+package com.mybatis3.dao.mapper;
+
+public interface StudentMapper {
+	/*
+	 * 인터페이스의 풀네임은 StudentMapper.xml의 namespace와일치
+	 * StudnetMapper 인터페이스의와 같은위치에 StudentMapper.xml 을 위치	
+	 * 메쏘드이름은 	StudentMapper.xml 파일의 id와일치
+	 * 메쏘드인자타입은 StudentMapper.xml 파일의 parameterType 와일치
+	 * 메쏘드리턴타입은 StudentMapper.xml 파일의 resultType 와일치(ResultSet이 1개이상일경우는 List)
+	 */
+}
